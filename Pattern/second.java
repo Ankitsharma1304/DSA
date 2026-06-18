2b9bba347b0b826c28d62f3abe3cc5a07d9e1626
Ankit Sharma <ankitsharma.13542@gmail.com>
2026-06-18 23:01:07 +0530

    pattern print

---
+++ Pattern/second.java
@@ -0,0 +1,13 @@
+package Pattern;
+
+public class second {
+    public static void main(String[] args) {
+        int n=3;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<5;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
