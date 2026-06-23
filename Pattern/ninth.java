66d31150ff01c14c7a406aa3dc81cfdb0784099c
Ankit Sharma <ankitsharma.13542@gmail.com>
2026-06-24 00:41:04 +0530

    SOLVE SOME MORE PATTERN PROGRAMS

---
+++ Pattern/ninth.java
@@ -0,0 +1,15 @@
+package Pattern;
+
+public class ninth {
+    public static void main(String[] args){
+        int n=3;
+        for(int i=1;i<=n;i++){
+            for (int j=1;j<=5;j++){
+                System.out.print("* ");
+            }
+            System.out.println();  
+        
+        
+        }
+    }
+}
